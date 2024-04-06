37280324a5d64969207b99865782614e27178a8c
Тимофей Кузин <timofejkuzin@MacBook-Pro-Timofej.local>
2024-04-06 23:56:57 +0200

    Commented line to reset database

--- src/main/java/com/example/uvs/Main.java
+++ src/main/java/com/example/uvs/Main.java
@@ -4,7 +4,7 @@ import com.example.uvs.GUI.LogInController;
 public class Main {
     //some information for login. If you want to see admin feature please put in login 'Admin' and in password 'qwerty123'
     public static void main(String[] args) {  //main class that starts an application
-        DataBaseConnection.DataBaseInterface.resetAllDatabase();   //you can use this method to restart all votings
+        //DataBaseConnection.DataBaseInterface.resetAllDatabase();   //you can use this method to restart all votings
         LogInController.main(args);  //it has to be in class main so Maven can build a jar file
     }
 }
